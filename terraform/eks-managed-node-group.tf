6ab1c3da83c56b65774802e5ab7fb51c23f79626
Rudimar Ronsoni Jr <rudimar@outlook.com>
2024-12-04 16:06:44 -0300

    update EKS configuration with specific cluster name and VPC settings; add main.tf and versions.tf for infrastructure setup

--- terraform/eks-managed-node-group.tf
+++ terraform/eks-managed-node-group.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "my-cluster"
+  cluster_name    = "eks-cluster-fiap-turma-7-grupo-48"
   cluster_version = "1.31"
 
   bootstrap_self_managed_addons = false
@@ -19,9 +19,9 @@ module "eks" {
   # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
-  vpc_id                   = "vpc-1234556abcdef"
-  subnet_ids               = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
-  control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
+  vpc_id                   = "vpc-fiap-turma-7-grupo-48"
+  subnet_ids               = ["subnet-fiap-turma-7-grupo-48-1", "subnet-fiap-turma-7-grupo-48-2", "subnet-fiap-turma-7-grupo-48-3"]
+  control_plane_subnet_ids = ["subnet-control-plane-fiap-turma-7-grupo-48-1", "subnet-control-plane-fiap-turma-7-grupo-48-2", "subnet-control-plane-fiap-turma-7-grupo-48-3"]
 
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
@@ -29,7 +29,7 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-    example = {
+    eks-cluster-fiap-turma-7-grupo-48 = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["m5.xlarge"]
@@ -40,8 +40,5 @@ module "eks" {
     }
   }
 
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
+  tags = local.tags
 }
